cc8ac3ab424c2604d4d103df0186274c2a23f44d
Ruramaimunashe Mutefura <rmutefura.23@westminster-mo.edu>
2021-07-05 23:20:40 +0000

    style fixes

--- stock_prices.py
+++ stock_prices.py
@@ -21,15 +21,13 @@ def menu():
 
 def fetchStockData(symbol):
     url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-chart"
-    
     headers = {
         'x-rapidapi-key': "4b9c386372msh305d0f5e33cb633p1cee26jsn477776e57ef8",
         'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
     }
 
     querystring = {"region": "US", "symbol": symbol, 
-                  "interval": "1d", "range": "3mo"}
-
+    "interval": "1d", "range": "3mo"}
     response = requests.get(url, headers=headers, params=querystring)
 
     if response.status_code == 200:
@@ -51,8 +49,10 @@ def parseTimestamp(inputdata):
 
 def parseValues(inputdata):
     valueList = []
-    valueList.extend(inputdata["chart"]["result"][0]["indicators"]["quote"][0]["open"])
-    valueList.extend(inputdata["chart"]["result"][0]["indicators"]["quote"][0]["close"])
+    valueList.extend(inputdata["chart"]["result"][0]
+    ["indicators"]["quote"][0]["open"])
+    valueList.extend(inputdata["chart"]["result"][0]
+    ["indicators"]["quote"][0]["close"])
     return valueList 
 
 
